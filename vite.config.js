99505e78027c2016f1e7f492806dc8384b48f482
thetanishqthakur <tanishqth83508@gmail.com>
2025-11-19 19:15:56 +0530

    Uncomment base path configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  // base: "/Portfolio/",
-=======
-  base: "/Portfolio/",
->>>>>>> 5e9a58ee84a3c98409fc245c60ee4cf4424807c9
+
 });
